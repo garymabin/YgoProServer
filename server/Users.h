ce2bea44196216e61443bbe34bf64946a85fb49b
zh99998 <zh99998@gmail.com>
2013-05-08 21:34:10 +0800

    allow wide characters in username

--- server/Users.h
+++ server/Users.h
@@ -12,13 +12,13 @@ namespace ygo
 
 struct UserData
 {
-    std::string username;
-    std::string password;
+    std::wstring username;
+    std::wstring password;
     unsigned int score;
     time_t  last_login;
-    UserData(std::string username,std::string password):username(username),password(password),score(1000){}
+    UserData(std::wstring username,std::wstring password):username(username),password(password),score(1000){}
     UserData():username("Player"),password(""),score(1000){}
-    UserData(std::string username,std::string password,unsigned int score):username(username),password(password),score(score){}
+    UserData(std::wstring username,std::wstring password,unsigned int score):username(username),password(password),score(score){}
 };
 
 
@@ -27,22 +27,22 @@ class Users
 {
     private:
     std::thread t1;
-    std::pair<std::string,std::string> splitLoginString(std::string);
-    bool validLoginString(std::string);
+    std::pair<std::wstring,std::wstring> splitLoginString(std::wstring);
+    bool validLoginString(std::wstring);
     static void SaveThread(Users*);
     std::mutex usersMutex;
     Users();
-    std::map<std::string,UserData*> users;
-    std::string getFirstAvailableUsername(std::string base);
+    std::map<std::wstring,UserData*> users;
+    std::wstring getFirstAvailableUsername(std::wstring base);
     void LoadDB();
     void SaveDB();
     public:
-    int getScore(std::string username);
+    int getScore(std::wstring username);
     static Users* getInstance();
-    std::string login(std::string);
-    std::string login(std::string,std::string);
-    void Victory(std::string, std::string);
-    void Victory(std::string, std::string,std::string, std::string);
+    std::wstring login(std::wstring);
+    std::wstring login(std::wstring,std::wstring);
+    void Victory(std::wstring, std::wstring);
+    void Victory(std::wstring, std::wstring,std::wstring, std::wstring);
 
 };
 
